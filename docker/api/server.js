ace2fdad3fbf49e070d98b568768ffa3e98626c4
Revenberg <sander@revenberg.info>
2025-08-08 08:59:20 +0200

    .

---
+++ docker/api/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'mysql',
+  user: 'meshtastic',
+  password: 'example_pw',
+  database: 'meshtastic'
+});
+
+app.post('/api/register', (req, res) => {
+  const { teamName, captainName } = req.body;
+  if (!teamName || !captainName) return res.status(400).send('Missing fields');
+  db.query(
+    'INSERT INTO teams (team_name, captain_name) VALUES (?, ?)',
+    [teamName, captainName],
+    (err) => {
+      if (err) return res.status(500).send('DB error');
+      res.status(200).send('OK');
+    }
+  );
+});
+
+app.get('/api/teams', (req, res) => {
+  db.query('SELECT id, team_name FROM teams', (err, results) => {
+    if (err) return res.status(500).send('DB error');
+    res.json(results);
+  });
+});
+
+// Add user registration endpoint
+app.post('/api/add_user', (req, res) => {
+  const { userName, teamId } = req.body;
+  if (!userName || !teamId) return res.status(400).send('Missing fields');
+  db.query(
+    'INSERT INTO users (user_name, team_id) VALUES (?, ?)',
+    [userName, teamId],
+    (err) => {
+      if (err) return res.status(500).send('DB error');
+      res.status(200).send('OK');
+    }
+  );
+});
+
+app.listen(3001, () => console.log('API server running on 3001'));
--- setup.sh
+++ setup.sh
@@ -21,6 +21,6 @@ HOST_IP=$(hostname -I | awk '{print $1}')
 # Vervang <HOST_IP> in index.html door het werkelijke IP-adres
 sed -i "s/<HOST_IP>/$HOST_IP/g" docker/webserver/index.html
 
-
-cd docker && docker compose up -d --build
+#cd docker && docker compose up -d --build
+cd docker && docker compose up --build
 echo "✅ Installatie voltooid"
