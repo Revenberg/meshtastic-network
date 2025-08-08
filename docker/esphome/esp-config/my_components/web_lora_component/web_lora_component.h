9a4b488ff9602f31340cff589c455e28a1037a2a
Revenberg <sander@revenberg.info>
2025-08-08 20:40:33 +0200

    .

---
+++ docker/esphome/esp-config/my_components/web_lora_component/web_lora_component.h
@@ -0,0 +1,27 @@
+#include "esphome.h"
+#include <SPI.h>
+#include <LoRa.h>
+
+namespace esphome {
+
+class WebLoraComponent : public Component {
+ public:
+  void setup() override {
+    // LoRa setup (adjust pins/frequency as needed)
+    LoRa.setPins(8, 14, 12); // NSS, RST, DIO0 (example for Heltec v3)
+    if (!LoRa.begin(868E6)) {
+      ESP_LOGE("lora", "Starting LoRa failed!");
+    } else {
+      ESP_LOGI("lora", "LoRa init OK");
+    }
+  }
+
+  void send_number(int value) {
+    ESP_LOGI("lora", "Sending number: %d", value);
+    LoRa.beginPacket();
+    LoRa.print(value);
+    LoRa.endPacket();
+  }
+};
+
+} // namespace esphome
