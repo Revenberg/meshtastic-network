535c64f25006207570f1d9afaae04015d26c20f8
Revenberg <sander@revenberg.info>
2025-08-10 20:29:25 +0200

    .

--- docker/esphome/esp-config/components/heltec_wifi_lora/heltec_wifi_lora.h
+++ docker/esphome/esp-config/components/heltec_wifi_lora/heltec_wifi_lora.h
@@ -3,7 +3,6 @@
 #include "esphome/core/component.h"
 #include "esphome/core/log.h"
 #include "heltec_wifi_lora.h"
-#include "network.h"
 #include <ArduinoJson.h>
 #include <LoRa.h>
 #include <WiFi.h>
